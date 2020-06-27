e6c989bf94838e6b568a9470f5fd302e93307dc8
Csilla <posa.csilla@gmail.com>
2020-06-27 18:04:21 +0200

    added trips to cities
    exporting data from Google Sheets table with JavaScript

---
+++ trips/trips_stavanger.js
@@ -0,0 +1,25 @@
+const tripListLink = "https://spreadsheets.google.com/feeds/list/1K1X4rkHQ8kxk9nD2r0hj55DpZzOWXhpuCaZs_zj13kk/od6/public/values?alt=json ";
+
+function loadJSON(link) {
+    fetch(tripListLink).then(e=>e.json()).then(data=>data.feed.entry.forEach(displayTrips));
+}
+
+const template = document.querySelector("template").content;
+
+function displayTrips(trips) {
+  
+    const clone = template.cloneNode("true");
+    clone.querySelector("img").setAttribute("src",`img/${trips.gsx$picture.$t}`)
+    clone.querySelector("#trip").textContent = trips.gsx$trips.$t;
+    clone.querySelector("#length").textContent = trips.gsx$length.$t;
+    clone.querySelector("#price").textContent = trips.gsx$price.$t;
+    clone.querySelector("#shortIntro").textContent = trips.gsx$description.$t;
+    
+    if (trips.gsx$location.$t === "Stavanger") {
+        document.querySelector("#stavanger").appendChild(clone);
+    }
+    
+}
+
+loadJSON(tripListLink);
+
